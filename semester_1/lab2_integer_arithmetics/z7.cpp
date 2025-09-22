0212fc9a493f479e137ba0b25bfcecf556a36dd0
markboostvadachoria <vadachoria@gmail.com>
2025-09-22 18:40:33 +0300

    add z7 lab2

---
+++ semester_1/lab2_integer_arithmetics/z7.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<cmath>
+
+bool pt(double n)
+{
+	
+	if (n - trunc(n) == 0)
+		return 1;
+	return 0;
+}
+
+int main()
+{
+	using std::cout;
+	using std::cin;
+	
+	int n;
+	cin >> n;
+	if (!cin || n < 0)
+		exit(1);
+
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j < i; j++)
+			if (pt(sqrt(i * i - j * j))) {
+				cout << j << ' ' << sqrt(i * i - j * j) << ' ' << i << '\n';
+				break;
+			}
+	
+	return 0;
+
+}
